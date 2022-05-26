355b766188fef24a996eefe9a59fb28a031c6994
Artyshook <Mkjbhg123>
2022-05-26 17:11:52 +0200

    Upload/ delete video files added

---
+++ client/src/bricks/AddVideoForm.js
@@ -0,0 +1,129 @@
+import {Button, Modal, Form} from "react-bootstrap";
+import {useState} from "react";
+import axios from "axios";
+
+export const AddVideoForm = (props) => {
+    const [formData, setFormData] = useState(false);
+    const [formDataVideo, setFormDataVideo] = useState()
+    const [id, setId]=useState()
+
+    // console.log(formData)
+    // console.log(formDataVideo)
+    // console.log(props.videoList.id)
+    console.log(id)
+
+    const createVideo = () => { // creating a new object with a title, then id is added on the backend side
+        fetch("video/create", {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+
+        })
+            .then(response => response.json())
+            .then(data=> {
+                props.setVideoList(current => {
+                    // const newList = current.slice();//making a copy of array
+                    // newList.push(data)
+                    // return newList
+                    return [...current, {data,isload:false}];
+                })
+                // props.setFormShown(false); - change after
+                setId(data.id)
+                alert(data.token) // show to the user a token for update/delete video
+            })
+    }
+
+
+    async function uploadVideo() {
+
+        const form_data = new FormData()
+        form_data.append("id", id)
+        form_data.append("file", formDataVideo)
+
+            for (let value of form_data.values()) {
+            console.log(value);
+        }
+
+        await axios.post('/video/upload', form_data, {
+             headers: {
+                "Content-Type": "multipart/form-data"
+            },
+        })
+        .then((response)=> {
+
+            console.log(response.data);
+            props.setVideoList(videoList =>{
+                return videoList.map(el =>{
+                    if ( el.id == response.data.id){
+                        el.isLoad=true
+                        return el
+                    }
+                    return el
+                });
+
+            });
+            // props.setVideoList(response.data)
+        })
+        .catch(error=>error)
+
+    }
+
+
+
+    // const mergingIdAndFile = () => {
+    //     let a = Object.keys(props.videoList).pop(); //getting id of the last added object/ should press "Save Changes" before adding a video file to get the latest id of an object from server
+    //     let b = formDataVideo //uploaded video
+    //     const c = {}
+    //     c.id = a
+    //     c.file = b
+    //     return c // merged id and video to a new variable for sending on server
+    // }
+    // console.log(mergingIdAndFile())
+
+
+    return (
+        <>
+
+            <Modal show={props.formShown} onHide={()=>props.setFormShown(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Enter a title for the video</Form.Label>
+                            <Form.Control as="textarea" rows={3}
+                                          onChange={(event)=>
+                                          {setFormData(current=>{
+                                              const newData = {...current};
+                                              newData.title = event.target.value;
+                                              return newData;
+                                          })}}
+                            />
+                        </Form.Group>
+                        <Button variant="primary" onClick={createVideo} >
+                            Save Changes
+                        </Button>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                        >
+                            <Form.Label>Add video</Form.Label>
+                            <Form.Control type="file"
+                                          onChange={(e) =>
+                                              setFormDataVideo(e.target.files[0])}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={uploadVideo}>
+                        Upload video
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
---
+++ client/src/bricks/VideoTile.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import {Button, Card, Col} from "react-bootstrap";
+import JoLPlayer from "jol-player";
+import DeliteVideo from "./DeliteVideo";
+import Icon from "@mdi/react";
+import {mdiPlus} from "@mdi/js";
+
+
+
+    export const VideoTile = (props) => {
+
+        const [formShown1, setFormShown1]=useState(false)
+
+    return (
+
+        <Col>
+            <Card>
+                <JoLPlayer
+                    option={{
+                        videoSrc:"/video/get-file?filepath=" + props.video.path, //getting video from the sever
+                        width: 400,
+                        height: 300,
+                        pausePlacement: "center",
+                        language: "en",
+                    }}
+                />
+                <Card.Body>
+                    <Card.Title>{props.video.title}</Card.Title>
+                    <Card.Text>
+                        Tags: {props.video.tagIds}
+                            <DeliteVideo formShown1={formShown1}
+                                         setFormShown1={setFormShown1}
+                                         videoID={props.video.id} // Video id for delete video
+                                         onClick={() => setFormShown1(true)}/>
+                        <Button variant="info" onClick={() => setFormShown1(true)}>
+                            Delete video
+                        </Button>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+
+    )}
